ed658dd5025ee816e869051f1e28ff4b66f4d5ec
billTas <billtas@gmail.com>
2025-06-30 02:34:25 +0300

    fix render object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,92 @@
-import Container from "./components/Container"
+import GeneralInfoForm from "./components/GeneralInfoForm";
+import EducationExperienceForm from "./components/EducationExperienceForm";
+import PracticalExperienceForm from "./components/PracticalExperienceForm";
+import Resume from "./components/Resume";
+import { useState } from "react";
 
+// const cvData = {
+//   general: { 
+//       name:"Bill",
+//       email:"bnbtrikala@gmail.com",
+//       phone:"",
+//   },
+//   education: [{
+//     schoolName:"",
+//     titleStudy:"",
+//     dateStudy:"",
+//   }],
+//   experience: [{
+//     companyName:"",
+//     positionTitle:"",
+//     responsibilities:"",
+//     from:"",
+//     until:"",
+//   }]
+// };
 
 
 function App() {
+ const [updateGeneralInfo, setUpdateGeneralInfo] = useState({
+    general: { 
+      name:"Bill",
+      email:"bnbtrikala@gmail.com",
+      phone:"6988878785",
+    },
+    education: [{
+      schoolName:"1",
+      titleStudy:"1",
+      dateStudy:"1",
+    }],
+    experience: [{
+      companyName:"Bnb",
+      positionTitle:"IT Technician",
+      responsibilities:"Fix Computers",
+      from:"",
+      until:"",
+    }]
+});
+
   return (
-    <Container />
-  )
+    <div className="container">
+      <header>
+        <h1>CV Builder</h1>
+      </header>
+      <div className="main-container">
+        <aside>
+          <h2>Your Info</h2>
+            <GeneralInfoForm />
+            <EducationExperienceForm updateEducation={updatelInfo}/>
+            <PracticalExperienceForm />
+        </aside>
+        <main>
+          <h2>Preview CV</h2>
+          <Resume 
+            personalInfo={updateGeneralInfo.general}
+            educationInfo={updateGeneralInfo.education}
+            experienceInfo={updateGeneralInfo.experience}
+            />
+        </main>
+      </div>
+            <footer>Bill Tas 2025</footer>
+    </div>
+    );
+  
+  function updatelInfo(event) {
+    event.preventDefault();
+    const schoolInput = document.querySelector('#school-name');
+    const titleofStudy = document.querySelector('#title-of-study');
+    const dateOfStudy = document.querySelector('#date-of-study');
+    // newEducation = {...updateGeneralInfo.education}
+    const newInfo = {...updateGeneralInfo, education: {...updateGeneralInfo.education, schoolName:schoolInput.value}};
+    setUpdateGeneralInfo(newInfo);
+    console.log(schoolInput.value);
+    console.log(newInfo);
+    console.log(updateGeneralInfo.education)
+  }
 }
 
+
 export default App
+
+ 
+ 
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,20 +4,22 @@ export default function Resume({personalInfo, educationInfo, experienceInfo}) {
       <div className="resume-top">
         <h1>Person Name</h1>
         <div className="personal-info">
-          {personalInfo}
+          {personalInfo.name} <br/>
+          {personalInfo.email} <br/>
+          {personalInfo.phone}
         </div>
       </div>
       <div className="resume-body">
         <div className="education-section">
           <h3>Education</h3>
           <div className="education">
-            {educationInfo}    
+            {educationInfo[0].schoolName}    
           </div>
         </div>
         <div className="experience-section">
           <h3>Professional experience</h3>
           <div className="experience">
-            {experienceInfo}
+            {experienceInfo[0].companyName}
           </div>
         </div>
       </div>
